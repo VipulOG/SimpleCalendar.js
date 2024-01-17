75aa0fef8fa421f19bc8e84ac5dd5a0f20e87422
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2024-01-17 22:29:49 +0530

    wip

--- src/main.js
+++ src/main.js
@@ -17,24 +17,60 @@ class SimpleCalendar {
 
   buildSelectedDateInfo() {
     const self = this;
+
     const container = document.createElement("div");
     container.classList.add("selected-date-info");
 
-    self.container.addEventListener("selectDate", (event) => {});
+    var selectedDay;
+    var selectedMonth;
+
+    self.container.addEventListener("selectDate", (event) => {
+      selectedMonth.textContent = event.detail.date.toLocaleString("default", {
+        month: "long",
+        year: "numeric",
+      });
+      selectedDay.textContent = event.detail.date.getDate();
+    });
+
+    function buildTodayButton() {
+      const todayButton = document.createElement("p");
+      todayButton.id = "today";
+      todayButton.textContent = "Today";
+      container.appendChild(todayButton);
+
+      todayButton.addEventListener("click", () => {
+        self.selectedDate = new Date();
+        self.renderedMonth = new Date();
+        self.container.dispatchEvent(
+          new CustomEvent("switchMonth", {
+            detail: { date: self.renderedMonth },
+          })
+        );
+        self.container.dispatchEvent(
+          new CustomEvent("selectDate", {
+            detail: { date: self.selectedDate },
+          })
+        );
+      });
+    }
 
     function buildSelectedDate() {
-      var selectedDateContainer = container.querySelector("selected-date");
+      const selectedDateContainer = document.createElement("div");
+      selectedDateContainer.classList.add("selected-date");
+      container.appendChild(selectedDateContainer);
 
-      if (selectedDateContainer) {
-        selectedDateContainer.innerHTML = "";
-      } else {
-        selectedDateContainer = document.createElement("div");
-        selectedDateContainer.classList.add("selected-date");
-        container.appendChild(selectedDateContainer);
-      }
+      selectedMonth = document.createElement("div");
+      selectedDay = document.createElement("div");
+      selectedMonth.id = "selected-month";
+      selectedDay.id = "selected-day";
 
-      const selectedMonth = document.createElement("div");
-      const selectedDay = document.createElement("div");
+      selectedMonth.textContent = self.selectedDate.toLocaleString("default", {
+        month: "long",
+        year: "numeric",
+      });
+
+      selectedDay.textContent = self.selectedDate.getDate();
+
       selectedDateContainer.appendChild(selectedMonth);
       selectedDateContainer.appendChild(selectedDay);
       container.appendChild(selectedDateContainer);
@@ -45,6 +81,7 @@ class SimpleCalendar {
       container.appendChild(eventsList);
     }
 
+    buildTodayButton();
     buildSelectedDate();
     buildEventsList();
     self.container.appendChild(container);
@@ -52,12 +89,15 @@ class SimpleCalendar {
 
   buildCalender() {
     const self = this;
+
     const container = document.createElement("div");
+    container.classList.add("calender-wrap");
+
     const calenderTable = document.createElement("table");
+    calenderTable.id = "calender";
 
     self.container.addEventListener("switchMonth", (event) => {
-      destroyBody();
-      buildBody();
+      buildDayCells();
     });
 
     self.container.addEventListener("selectDate", (event) => {
@@ -65,17 +105,19 @@ class SimpleCalendar {
       if (selectedDate) {
         selectedDate.classList.remove("selected");
       }
-      calenderTable.querySelectorAll(".day")[event.detail.date.getDate() - 1].classList.add("selected");
+      const days = calenderTable.querySelectorAll(".day");
+      const selectedDayIndex = event.detail.date.getDate() - 1;
+      days[selectedDayIndex].classList.add("selected");
     });
 
-    function buildMonthSelector() {
-      const monthSelector = document.createElement("div");
-      monthSelector.classList.add("month-selector");
-      container.appendChild(monthSelector);
+    function buildMonthSwitch() {
+      const monthSwitch = document.createElement("div");
+      monthSwitch.classList.add("month-switch");
+      container.appendChild(monthSwitch);
 
       const prevMonthButton = document.createElement("button");
       prevMonthButton.textContent = "<";
-      monthSelector.appendChild(prevMonthButton);
+      monthSwitch.appendChild(prevMonthButton);
 
       prevMonthButton.addEventListener("click", () => {
         self.renderedMonth = new Date(
@@ -93,7 +135,7 @@ class SimpleCalendar {
 
       const nextMonthButton = document.createElement("button");
       nextMonthButton.textContent = ">";
-      monthSelector.appendChild(nextMonthButton);
+      monthSwitch.appendChild(nextMonthButton);
 
       nextMonthButton.addEventListener("click", () => {
         self.renderedMonth = new Date(
@@ -125,17 +167,23 @@ class SimpleCalendar {
       });
     }
 
-    function destroyBody() {
-      const body = calenderTable.querySelector("tbody");
-      if (body) {
-        body.parentNode.removeChild(body);
-      }
-    }
-
     function buildBody() {
       const body = document.createElement("tbody");
       calenderTable.appendChild(body);
 
+      for (let i = 0; i < 7; i++) {
+        const row = document.createElement("tr");
+        for (let j = 0; j < 7; j++) {
+          const cell = document.createElement("td");
+          row.appendChild(cell);
+        }
+        body.appendChild(row);
+      }
+
+      buildDayCells();
+    }
+
+    function buildDayCells() {
       const daysInMonth = new Date(
         self.renderedMonth.getFullYear(),
         self.renderedMonth.getMonth() + 1,
@@ -148,45 +196,40 @@ class SimpleCalendar {
         1
       ).getDay();
 
-      let dayCounter = 1;
-      for (let i = 0; i < 7; i++) {
-        const row = document.createElement("tr");
-        for (let j = 0; j < 7; j++) {
-          const cell = document.createElement("td");
-          if (i === 0 && j < firstDayIndex) {
-            // Empty cell for days before the 1st
-          } else if (dayCounter <= daysInMonth) {
-            cell.appendChild(buildDayCell(dayCounter));
-            dayCounter++;
-          }
-          row.appendChild(cell);
-        }
-        body.appendChild(row);
+      const cells = calenderTable.querySelectorAll("td");
+
+      // clear all cells
+      cells.forEach((cell) => {
+        cell.innerHTML = "";
+        cell.classList.remove("day");
+        cell.classList.remove("today");
+        cell.classList.remove("selected");
+      });
+
+      // fill in cells
+      for (let i = 0; i < daysInMonth; i++) {
+        const cell = cells[firstDayIndex + i];
+        cell.classList.add("day");
+        cell.textContent = i + 1;
+
+        const isSelected =
+          self.renderedMonth.getMonth() === self.selectedDate.getMonth() &&
+          self.renderedMonth.getFullYear() ===
+            self.selectedDate.getFullYear() &&
+          self.selectedDate.getDate() === i + 1;
+
+        const isToday =
+          self.renderedMonth.getMonth() === self.today.getMonth() &&
+          self.renderedMonth.getFullYear() === self.today.getFullYear() &&
+          self.today.getDate() === i + 1;
+
+        if (isSelected) cell.classList.add("selected");
+        if (isToday) cell.classList.add("today");
+
+        cell.addEventListener("click", () => {
+          selectDay(i + 1);
+        });
       }
-    }
-
-    function buildDayCell(day) {
-      const cell = document.createElement("div");
-
-      const isSelected =
-        self.renderedMonth.getMonth() === self.selectedDate.getMonth() &&
-        self.renderedMonth.getFullYear() === self.selectedDate.getFullYear() &&
-        self.selectedDate.getDate() === day;
-
-      const isToday =
-        self.renderedMonth.getMonth() === self.today.getMonth() &&
-        self.renderedMonth.getFullYear() === self.today.getFullYear() &&
-        self.today.getDate() === day;
-
-      cell.textContent = day;
-      cell.classList.add("day");
-
-      if (isSelected) cell.classList.add("selected");
-      if (isToday) cell.classList.add("today");
-
-      cell.addEventListener("click", () => selectDay(day));
-
-      return cell;
     }
 
     function selectDay(day) {
@@ -203,7 +246,7 @@ class SimpleCalendar {
       );
     }
 
-    buildMonthSelector();
+    buildMonthSwitch();
     buildHeader();
     buildBody();
 
